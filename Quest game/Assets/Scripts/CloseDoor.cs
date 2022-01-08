64ac1fd4e3368bb0c51d739e1b9278ec17085a73
Konstantin <kostya.300397@gmail.com>
2022-01-08 15:58:18 +0500

    Дз 3, добавил спавнер объектов, поменял скрипт перемещения, добавил загадку в первой комнате

---
+++ Quest game/Assets/Scripts/CloseDoor.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoor : MonoBehaviour
+{
+    [SerializeField] private GameObject book;
+    [SerializeField] private Transform spawnPoint;
+    public GameObject Door;
+    private bool doorClose;
+    private int speed = 1;
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            doorClose = true;
+            Instantiate(book, new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z), Quaternion.Euler(0,-90,0));
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+    private void Update()
+    {
+        if (doorClose) {
+
+            Door.transform.position = Vector3.MoveTowards(Door.transform.position, new Vector3(5, 0.5f, 0), speed * Time.deltaTime);
+            if (Door.transform.position.z <= 0) {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
