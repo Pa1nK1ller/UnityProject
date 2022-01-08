afac35e38ec7c1078a8e96e5093d5cd0523776c8
Konstantin <kostya.300397@gmail.com>
2022-01-08 15:58:18 +0500

    Дз 3, добавил спавнер объектов, поменял скрипт перемещения, добавил загадку в первой комнате

---
+++ Quest game/Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    [SerializeField] private GameObject Door;
+    bool enter;
+    int speed = 1;
+    bool open;
+    private void Start()
+    {
+        Door = GameObject.FindGameObjectWithTag("DoorQestRoom1");
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            enter = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            enter = false;
+        }
+
+    }
+    private void OnGUI()
+    {
+        if (enter)
+        {
+            GUI.Label(new Rect(Screen.width / 2 - 75, Screen.height - 100, 150, 100), "Нажмите F чтобы использовать");
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown("f") && enter)
+        {
+            open = true;  
+        }
+
+        if (open)
+        {
+            Door.transform.position = Vector3.MoveTowards(Door.transform.position, new Vector3(5, 0.5f, 1.1f), speed * Time.deltaTime);
+            if (Door.transform.position.z >= 1.1f)
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ Quest game/Assets/Scripts/SpawnItems.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnItems : MonoBehaviour
+{
+    public GameObject key;
+    public GameObject spawnPoint;
+
+    void Start()
+    {
+        Instantiate(key,new Vector3(spawnPoint.transform.position.x,spawnPoint.transform.position.y,spawnPoint.transform.position.z),Quaternion.Euler(0,90,0));
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
